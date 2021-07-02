4dc6d1fe0b49bbdd78dded1edd40a053ddaa7459
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-02 16:03:15 +0300

    created new structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-import Header from "./Header";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app-wrapper">
       <Header />
+      <Navbar />
+      <Profile />
+      <footer className="footer"></footer>
     </div>
   );
 };
