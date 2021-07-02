9f0096fda2603e6d6d7c0602a257239bf1584777
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-02 16:16:03 +0300

    Revert "Revert "New stucture""
    This reverts commit 0d91b4d750022c784f2251bac16cc9fc1bc3b107.

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
+      <footer className="footer"></footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <a href="#s">contentment</a>
+    </div>
+  );
+};
+
+export default Profile;
