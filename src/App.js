1a0dfba4598cfd284ae2f40312ee4f0e0dc006f9
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-07-02 14:40:47 +0300

    info modules, import, export, componenta - teg

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-    </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <div>
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
     </div>
   );
 };
