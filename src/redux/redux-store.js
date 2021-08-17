aef90c33e1bda471c9f6ddcf65c1874d42691310
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-08-17 14:37:26 +0300

    added redux-form and create login component

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,7 @@ import profileReducer from "./profile-reducer"
 import sidebarReducer from "./sidebar-reducer"
 import usersReducer from "./users-reducer";
 import thunkMiddleware from "redux-thunk"
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
      profilePage: profileReducer,
@@ -13,6 +14,7 @@ let reducers = combineReducers({
      sidebar: sidebarReducer,
      usersPage: usersReducer,
      auth: authReducer,
+     form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware)); // создаем Store и закидываем туда наш state/ и внедряем  Middleware   
