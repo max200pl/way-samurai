81d307cb53ca37cacecd622383f6f5a3d6be9ff4
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-08-18 10:36:06 +0300

    added redux-rom for MyPost container

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 import { profileAPI } from "../api/api";
 
 const ADD_POST = "ADD-POST";
-const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 const SET_USER_PROFILE = "SET_USER_PROFILE";
 const SET_STATUS = "SET_STATUS";
 
@@ -10,7 +9,6 @@ let initialState = {
           { id: 1, message: "hi, how are you", likeCount: 12 },
           { id: 2, message: "Its my first post", likeCount: 10 },
      ],
-     newPostText: "new post text",
      profile: null,
      status: ""
 }
@@ -21,22 +19,14 @@ const profileReducer = (state = initialState, action) =>
           case ADD_POST: {
                let newPost = {
                     id: 5,
-                    message: state.newPostText,
+                    message: action.newPostTex,
                     likeCount: 0
                };
 
                return {
                     ...state,
                     postData: [...state.postData, newPost],
-                    newPostText: '',
                };
-          }
-
-          case UPDATE_NEW_POST_TEXT: {
-               return {
-                    ...state,
-                    newPostText: action.newText
-               }
           }
 
           case SET_USER_PROFILE: {
@@ -58,9 +48,7 @@ const profileReducer = (state = initialState, action) =>
      }
 }
 //* ФУНКЦИИ ВСПОМОГАТЕЛЬНЫЕ - для передачи типа action для dispatch(action)
-export const addPostActionCreator = () => ({ type: ADD_POST, })
-export const updateNewPostTextActionCreator = (text) =>
-     ({ type: UPDATE_NEW_POST_TEXT, newText: text })
+export const addPostActionCreator = (newPostTex) => ({ type: ADD_POST, newPostTex })
 
 export const setUserProfile = (profile) =>
      ({ type: SET_USER_PROFILE, profile })
