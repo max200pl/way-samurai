12dc28f1f2d30e99b05066c45207cac536d345e8
Maksym Poskannyi <47009755+max200pl@users.noreply.github.com>
2021-08-20 16:25:51 +0300

    83 addd reselect library

--- src/redux/users-selectors.js
+++ src/redux/users-selectors.js
@@ -1,7 +1,18 @@
-export const getUsers = (state) =>
+import { createSelector } from "reselect"
+
+
+const getUsersSelector = (state) =>
 {
      return state.usersPage.users
 }
+
+export const getUsers = createSelector(getUsersSelector, (users) =>
+// Принимает на вход selector и данные с примитивного selector 
+// кэширует данные и при следующем обращении к селектору не вызывает его а просто отдает данные 
+// можно вкладывать сложные селекторы в сложные селекторы 
+{
+     return users.filter(u => true)
+})
 
 export const getPageSize = (state) =>
 {
